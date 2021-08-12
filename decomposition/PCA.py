6806c028cb08734a949a1eb96abeb473ae5898e0
V Ng <vee.vidify@gmail.com>
2021-08-12 17:59:22 +1000

    update to algo

--- decomposition/PCA.py
+++ decomposition/PCA.py
@@ -23,16 +23,29 @@ def PCA(X):
 
     return eigvals, eigvecs, explained_variances
 
-def calc_major_component(x, q, eigvecs, eigvals, explained_variances):
-    # obs x
+def calc_principle_components(X_mean, X, eigvecs):
+    # X_mean px1 is mean vector of orig matrix nxp
+    # X is col vec of an obs or matrix of col vecs pxn, each col is an obs
+    # eigvecs is pxp of orig matrix
+
+    X_mc = X - X_mean
+    # pci = eigveci.T dot Xj -> Xj has pc vector px1
+    return eigvecs.T.dot(X)
+
+def calc_major_metrics(X_PC, q, eigvals):
+    # obs matrix's PCA (cols as obs) pxn
     # major threshold q < p
-    # PCA of X
+    # eigvals of orig X
+
+    # each Xj: sigma(pci**2 / lambdai) i: 1 -> q
     pass
 
-def calc_minor_component(x, r, eigvecs, eigvals, explained_variances):
-    # obs x
-    # major threshold q < p
-    # PCA of X
+def calc_minor_metrics(X_PC, r, eigvals):
+    # obs matrix's PCA (cols as obs) pxn
+    # minor threshold r < p
+    # PCA and mean of orig X
+
+    # each Xj: sigma(pci**2 / lambdai) i: r+1 -> p
     pass
 
 def preprocess_anomaly_metrics(X):
@@ -43,17 +56,19 @@ def preprocess_anomaly_metrics(X):
 
     # PREPROCESS
     # calculate p PCs for each obs Xj of X_feat:
-    # pci = eigveci.T dot Xj -> Xj has pc vector px1
     # -> PC matrix pxn of X
+    X_mean = X.mean(axis=0).reshape((1, -1)) # mean of each feat, as col vec px1
+    X_feat = X.T # now cols = obs, rows = feats -> pxn
+    X_PC = calc_principle_components(X_mean, X, eigvecs) # pxn
 
     # SELECT C1 THRESHOLD:
     # find q = # of first principle components explaining 50% variance, using eigvals
-    # calculate major metric for each Xj: sigma(yi**2 / lambdai) i: 1 -> q
+    # calculate major metric for each Xj
     # sort by metrics, desc, take 95% percentile
 
     # SELECT C2 THRESHOLD:
     # find r: eigvals lambda i: r+1 -> p: lambdai < 0.2
-    # calculate minor metric for each Xj: sigma(yi**2 / lambdai) i: r+1 -> p
+    # calculate minor metric for each Xj
     # sort by metrics, desc, take 95% percentile
 
     # return c1, c2
@@ -63,11 +78,11 @@ def preprocess_anomaly_metrics(X):
 def anomaly_classification(x0, c1, c2, eigvecs, eigvals):
     # CLASSIFICATION for x0
     # get p pcs of x0: pci = eigveci.T dot x0 i: 1 -> p
-
-    # calculate major component metric sigma(yi**2 / lambdai) i: 1 -> q
+    # calculate major components metric: sigma(yi**2 / lambdai) i: 1 -> q
     # calculate minor components metric: sigma(yi**2 / lambdai) i: r+1 -> p
-    # decision anomaly if major > c1 or minor > c2
-    # otherwise normal
+
+    # decision: anomaly if major > c1 or minor > c2
+    # normal otherwise
 
     # return classification label: 1 if normal, -1 if anomaly
 
