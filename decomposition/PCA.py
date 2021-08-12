01f3f9f8de733148aabc43bfd21d16a360cb5c18
V Ng <vee.vidify@gmail.com>
2021-08-12 18:42:04 +1000

    implement major and minor metrics calc. c1 c2 quantile-based selection

--- decomposition/PCA.py
+++ decomposition/PCA.py
@@ -30,23 +30,40 @@ def calc_principle_components(X_mean, X, eigvecs):
 
     X_mc = X - X_mean
     # pci = eigveci.T dot Xj -> Xj has pc vector px1
-    return eigvecs.T.dot(X)
+    return eigvecs.T.dot(X_mc)
 
 def calc_major_metrics(X_PC, q, eigvals):
     # obs matrix's PCA (cols as obs) pxn
     # major threshold q < p
     # eigvals of orig X
 
-    # each Xj: sigma(pci**2 / lambdai) i: 1 -> q
-    pass
+    # each col Xj: sigma(pci**2 / lambdai) i: 1 -> q
+    n = X_PC.shape[1]
+    metrics = np.zeros(n) # init empty array of metric for each obs
+    for j in range(n):
+        s = 0
+        for i in range(q):
+            s = s + X_PC[i][j]**2 / eigvals[i]
+        metrics[j] = s
+
+    return metrics
 
 def calc_minor_metrics(X_PC, r, eigvals):
     # obs matrix's PCA (cols as obs) pxn
     # minor threshold r < p
     # PCA and mean of orig X
 
-    # each Xj: sigma(pci**2 / lambdai) i: r+1 -> p
-    pass
+    # each col Xj: sigma(pci**2 / lambdai) i: r+1 -> p
+    n = X_PC.shape[1]
+    p = X_PC.shape[0]
+    metrics = np.zeros(n) # init empty array of metric for each obs
+    for j in range(n):
+        s = 0
+        for i in range(r+1, p):
+            s = s + X_PC[i][j]**2 / eigvals[i]
+        metrics[j] = s
+
+    return metrics
 
 def preprocess_anomaly_metrics(X):
     scaler = StandardScaler()
@@ -59,21 +76,33 @@ def preprocess_anomaly_metrics(X):
     # -> PC matrix pxn of X
     X_mean = X.mean(axis=0).reshape((1, -1)) # mean of each feat, as col vec px1
     X_feat = X.T # now cols = obs, rows = feats -> pxn
-    X_PC = calc_principle_components(X_mean, X, eigvecs) # pxn
+    X_PC = calc_principle_components(X_mean, X_feat, eigvecs) # pxn
 
     # SELECT C1 THRESHOLD:
     # find q = # of first principle components explaining 50% variance, using eigvals
     # calculate major metric for each Xj
     # sort by metrics, desc, take 95% percentile
+    sum_variances = 0
+    n = X_feat.shape[1]
+    p = X_feat.shape[0]
+    for i in range(p):
+        sum_variances = sum_variances + explained[i]
+        if sum > 0.5:
+            q = i
+            break
+    major_metrics = calc_major_metrics(X_PC, q, eigvals)
+    # quantile
+    c1 = np.quantile(major_metrics, 0.05) # 15% outliers
 
     # SELECT C2 THRESHOLD:
     # find r: eigvals lambda i: r+1 -> p: lambdai < 0.2
     # calculate minor metric for each Xj
     # sort by metrics, desc, take 95% percentile
+    r = np.searchsorted(eigvals, 0.2)
+    minor_metrics = calc_minor_metrics(X_PC, r, eigvals)
+    c2 = np.quantile(minor_metrics, 0.05) # 15% outliers
 
-    # return c1, c2
-
-    pass
+    return c1, c2
 
 def anomaly_classification(x0, c1, c2, eigvecs, eigvals):
     # CLASSIFICATION for x0
