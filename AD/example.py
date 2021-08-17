implement gradient decision function for pcaad. fix up some contour plots for examples

AD/example.py
@@ -64,12 +64,16 @@ def ocsvm():
         plot_space = np.c_[xx.ravel(), yy.ravel()]
         # np.set_printoptions(threshold=sys.maxsize)
         # print(plot_space)
+
+        Z = oc_svm.predict(plot_space)
+        Z_contours = Z.reshape(xx.shape)
+        plt.contour(xx, yy, Z_contours, levels=[0], linewidths=2, colors='black')
+
         Z = oc_svm.decision_function(plot_space)
         Z_contours = Z.reshape(xx.shape)
 
         # print(Z)
-        plt.contour(xx, yy, Z_contours, levels=[0], linewidths=2, colors='black')
-        plt.contourf(xx, yy, Z_contours, levels=np.linspace(Z_contours.min(), Z_contours.max(), 7), cmap=plt.cm.Blues_r)
+        plt.contourf(xx, yy, Z_contours, levels=np.linspace(Z_contours.min(), Z_contours.max(), 8), cmap=plt.cm.Blues_r)
         plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[(labels+1) // 2])
 
     plt.show()
@@ -104,17 +108,23 @@ def iforest():
         plt.subplot(1, len(ds), dataset_i+1)
 
         iforest = IsolationForest(contamination=outlier_percentage, random_state=42)
-        labels = iforest.fit_predict(X)
+        iforest.fit(X)
+        labels = iforest.predict(X)
+        print(labels.shape)
 
         plot_space = np.c_[xx.ravel(), yy.ravel()]
         # np.set_printoptions(threshold=sys.maxsize)
         # print(plot_space)
+
+        Z = iforest.predict(plot_space)
+        Z_contours = Z.reshape(xx.shape)
+        plt.contour(xx, yy, Z_contours, levels=[0], linewidths=2, colors='black')
+
         Z = iforest.decision_function(plot_space)
         Z_contours = Z.reshape(xx.shape)
 
         # print(Z)
-        plt.contour(xx, yy, Z_contours, levels=[0], linewidths=2, colors='black')
-        plt.contourf(xx, yy, Z_contours, levels=np.linspace(Z_contours.min(), Z_contours.max(), 7), cmap=plt.cm.Blues_r)
+        plt.contourf(xx, yy, Z_contours, levels=np.linspace(Z_contours.min(), Z_contours.max(), 8), cmap=plt.cm.Blues_r)
         plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[(labels+1) // 2])
 
     plt.show()
@@ -149,7 +159,7 @@ def gaussian_mixture():
         # print(Z)
         # contours = plt.contour(xx, yy, Z_contours, linewidths=2)
         # plt.colorbar(contours, shrink=0.8, extend='both')
-        plt.contourf(xx, yy, Z_contours, levels=np.linspace(Z_contours.min(), Z_contours.max(), 7), cmap=plt.cm.Blues_r)
+        plt.contourf(xx, yy, Z_contours, levels=np.linspace(Z_contours.min(), Z_contours.max(), 8), cmap=plt.cm.Blues_r)
         plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[(labels+1) // 2])
 
     plt.show()
@@ -184,7 +194,7 @@ def kde():
         # print(Z)
         # contours = plt.contour(xx, yy, Z_contours, linewidths=2)
         # plt.colorbar(contours, shrink=0.8, extend='both')
-        plt.contourf(xx, yy, Z_contours, levels=np.linspace(Z_contours.min(), Z_contours.max(), 7), cmap=plt.cm.Blues_r)
+        plt.contourf(xx, yy, Z_contours, levels=np.linspace(Z_contours.min(), Z_contours.max(), 8), cmap=plt.cm.Blues_r)
         plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[(labels+1) // 2])
 
     plt.show()
@@ -202,17 +212,21 @@ def pcaad():
         pcaad = PCAAD()
         pcaad.fit(X)
         labels = pcaad.predict(X)
-
         print(labels.shape)
 
         plot_space = np.c_[xx.ravel(), yy.ravel()]
         # np.set_printoptions(threshold=sys.maxsize)
         # print(plot_space)
+
         Z = pcaad.predict(plot_space)
-        # Z_contours = Z.reshape(xx.shape)
+        Z_contours = Z.reshape(xx.shape)
+        plt.contour(xx, yy, Z_contours, levels=[0], linewidths=2, colors='black')
+
+        Z = pcaad.decision_function(plot_space)
+        Z_contours = Z.reshape(xx.shape)
 
         # print(Z)
-        # plt.contour(xx, yy, Z_contours, levels=[0], linewidths=2, colors='black')
+        plt.contourf(xx, yy, Z_contours, levels=np.linspace(Z_contours.min(), Z_contours.max(), 8), cmap=plt.cm.Blues_r)
         plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[(labels+1) // 2])
 
     plt.show()