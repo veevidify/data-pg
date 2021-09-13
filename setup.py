c4d7fce4b91846187638375bbb25261f3fad4535
V Ng <vee.vidify@gmail.com>
2021-09-13 14:44:16 +1000

    add setuptools to use modules in notebooks

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="pg",
+    version="0.0.1",
+    description="Personal ML playground",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6.9',
+)
